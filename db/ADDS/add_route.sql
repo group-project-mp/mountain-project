1cd594104f439a4c658224267484be7b9caef36e
brittanyjat <33502518+brittanyjat@users.noreply.github.com>
2018-04-11 10:11:24 -0600

    add route/location now functinoal

--- db/ADDS/add_route.sql
+++ db/ADDS/add_route.sql
@@ -1,8 +1,9 @@
 WITH ins as (
    INSERT INTO routes
-       (latitude, longitude, name, pitches, rating, star_votes, stars, type)
+       (image, latitude, longitude, name, pitches, rating, star_votes, stars, type)
    VALUES
-       ( $1, $2, $3, $4, $5, $6, $7, $8 )
-   RETURNING name, route_id )
+       ( $1, $2, $3, $4, $5, $6, $7, $8, $9 )
+   RETURNING name, id )
 INSERT INTO locations (slot_1, slot_2, slot_3, slot_4, slot_5, slot_6, route_name, route_id )
-   SELECT $9, $10, $11, $12, $13, $14, route_name, route_id FROM ins;
+   SELECT $10, $11, $12, $13, $14, $15, name, id FROM ins;
+
