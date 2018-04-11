c1e8464effe2fbd7872e9438560301cd599eb0ec
marcusko77 <marcusko77@gmail.com>
2018-04-11 14:02:24 -0600

    api call for routes

--- db/ADDS/add_route.sql
+++ db/ADDS/add_route.sql
@@ -4,5 +4,5 @@ WITH ins as (
   VALUES
       ( $1, $2, $3, $4, $5, $6, $7, $8, $9, $10 )
   RETURNING name, id )
-INSERT INTO locations (slot_1, slot_2, slot_3, slot_4, slot_5, slot_6, route_name, route_id )
-  SELECT $11, $12, $13, $14, $15, $16, name, id FROM ins;
+INSERT INTO locations (slot_1, slot_2, slot_3, slot_4, slot_5, slot_6, route_name, route_id, mp_id )
+  SELECT $11, $12, $13, $14, $15, $16, name, id, mp_id FROM ins;
