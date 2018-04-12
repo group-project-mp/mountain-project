1c10235de6a6cbfa272fbd0c815ba87b8c048491
brittanyjat <33502518+brittanyjat@users.noreply.github.com>
2018-04-12 15:38:52 -0600

    Created add new route form. Added function in route reducer to handle inputs

--- src/ducks/routes.js
+++ src/ducks/routes.js
@@ -1,10 +1,43 @@
 const initialState = {
-    routes: []
+    routes: [],
+    name: '',
+    pitches: 0,
+    Type: '',
+    Difficulty: '',
+    topRope: false,
+    rating: 1,
+    description: '',
+    protection: '',
+    image: '',
+    latitude: '',
+    longitude: '',
+    state: '',
+    slot_2: '',
+    slot_3: '',
+    slot_4: '',
+    slot_5: '',
+    typeOptions: [
+        { key: 'Trad', value: 'Trad', text: 'Trad' },
+        { key: 'Sport', value: 'Sport', text: 'Sport' },
+        { key: 'Boulder', value: 'Boulder', text: 'Boulder' }],
+    typeSelected: null,
 }
 
+const ADDINPUT = 'ADDINPUT';
+
 export default function reducer(state = initialState, action) {
+    const { payload } = action;
     switch (action.type) {
+        case ADDINPUT:
+            return { ...state, [payload.prop]: payload.val }
         default:
             return state;
+    }
+}
+
+export function handleInput(prop, val) {
+    return {
+        type: ADDINPUT,
+        payload: { prop, val }
     }
 }
