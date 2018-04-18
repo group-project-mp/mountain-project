869eaa09c5fc28d97aecba179c0b63efe914e46e
brittanyjat <33502518+brittanyjat@users.noreply.github.com>
2018-04-18 12:35:14 -0600

    styling route detail

--- src/components/Route/ImageTicks.js
+++ src/components/Route/ImageTicks.js
@@ -6,20 +6,23 @@ export default function ImageTicks(props) {
         <div className='similar-tick'>
             <div className='you-and-this-route'>
                 <h3>{'You & This Route'}</h3>
-                <div>
-                    <h4>Your To-Do List:</h4>
-                    <span>Add To-Do</span>
+                <div className='you-sub'>
+                    <span style={{fontWeight: 'bold', fontSize: '16px'}}>Your To-Do List:</span>
+                    <span style={{cursor: 'pointer'}}>Add To-Do</span>
                 </div>
-                <div>
-                    <h4>Your Star Rating:</h4>
+                <div className='you-sub'>
+                    <span style={{fontWeight: 'bold'}}>Your Star Rating:</span>
                     <Rating maxRating={5} icon='star' onRate={null} size='massive' />
                 </div>
-                <div>
-                    <h4>Your Difficulty Rating:</h4>
+                <div className='you-sub'>
+                    <span style={{fontWeight: 'bold'}}>Your Difficulty Rating:</span>
+                    <select>
+                        <option>test</option>
+                    </select>
                 </div>
-                <div>
-                    <h4>Your Ticks:</h4>
-                    <span>Add New Tick</span>
+                <div className='you-sub'>
+                    <span style={{fontWeight: 'bold'}}>Your Ticks:</span>
+                    <span style={{cursor: 'pointer'}}>Add New Tick</span>
                 </div>
             </div>
 
